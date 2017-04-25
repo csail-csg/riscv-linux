f4a93c3aef381d2831a0054f0f24361b3ab93cce
Sizhuo Zhang <szzhang@mit.edu>
2017-04-25 15:24:50 -0400

    add missing SFENCE.VM

--- arch/riscv/include/asm/cacheflush.h
+++ arch/riscv/include/asm/cacheflush.h
@@ -11,6 +11,18 @@ static inline void local_flush_icache_all(void)
 	asm volatile ("fence.i" ::: "memory");
 }
 
+#undef flush_cache_vmap
+static inline void flush_cache_vmap(unsigned long start, unsigned long end)
+{
+    asm volatile ("sfence.vm" ::: "memory");
+}
+
+#undef flush_cache_vunmap
+static inline void flush_cache_vunmap(unsigned long start, unsigned long end)
+{
+    asm volatile ("sfence.vm" ::: "memory");
+}
+
 #ifndef CONFIG_SMP
 
 #define flush_icache_range(start, end) local_flush_icache_all()
